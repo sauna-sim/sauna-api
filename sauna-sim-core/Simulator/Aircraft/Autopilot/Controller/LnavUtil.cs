4711c7271fbb585719a8bc5b7f9e11fb4dbdf98b
Samuel Rey <samuelrparaja@gmail.com>
2023-10-08 02:36:47 +0200

    added LnavUtil

---
+++ sauna-sim-core/Simulator/Aircraft/Autopilot/Controller/LnavUtil.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaunaSim.Core.Simulator.Aircraft.Autopilot.Controller
+{
+    public static class LnavUtil
+    {
+
+    }
+}
