67561dea0e7e37a94559e839eb75b3dcea330183
Prithvi Shivaraman <prithvisagar.shivaraman@gmail.com>
2023-10-16 23:39:23 -0400

    Added test project

---
+++ sauna-tests/SaunaApiTestWebApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace sauna_tests
+{
+	public class SaunaApiTestWebApp : WebApplicationFactory<SaunaSim.Api.Program>
+	{
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            // shared extra set up goes here
+            return base.CreateHost(builder);
+        }
+    }
+}
+
