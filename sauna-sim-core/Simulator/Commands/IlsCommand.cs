9d9af3c3f6fd8f02df065cf22691951f4ba63fe7
Samuel Rey Paraja <UO295400@uniovi.es>
2023-11-23 17:38:35 +0100

    i must be going insane (fixed merge)

--- sauna-sim-core/Simulator/Commands/IlsCommand.cs
+++ sauna-sim-core/Simulator/Commands/IlsCommand.cs
@@ -24,12 +24,20 @@ namespace SaunaSim.Core.Simulator.Commands
         
         public void ExecuteCommand()
         {
-            // Create localizer (just a CourseToFixLeg) and glidepath (setting the angleConstraint on the leg)
-            CourseToFixLeg locLeg = new CourseToFixLeg(new FmsPoint(new RouteWaypoint(_loc.Loc_location), RoutePointTypeEnum.FLY_OVER), BearingTypeEnum.MAGNETIC, _loc.Loc_bearing);
-            locLeg.EndPoint.AngleConstraint = 3.0;
+            if (Aircraft.Autopilot.CurrentLateralMode == LateralModeType.LNAV)
+            {
+                Aircraft.Autopilot.SelectedHeading = (int)Aircraft.Position.Heading_Mag;
+            }
 
-            // TODO: This should be threshold elevation
-            locLeg.EndPoint.Point.PointPosition.Alt = 0;
+            // Add the LOC leg
+            IRoutePoint locRoutePoint = new RouteWaypoint("LOC" + _loc.Runway_identifier, _loc.Loc_location);
+            FmsPoint locFmsPoint = new FmsPoint(locRoutePoint, RoutePointTypeEnum.FLY_OVER)
+            {
+                LowerAltitudeConstraint = _loc.Glideslope.Gs_elevation,
+                UpperAltitudeConstraint = _loc.Glideslope.Gs_elevation,
+                AngleConstraint = _loc.Glideslope.Gs_angle
+            };
+            CourseToFixLeg locLeg = new CourseToFixLeg(locFmsPoint, BearingTypeEnum.MAGNETIC, _loc.Loc_bearing);
 
             Aircraft.Fms.AddRouteLeg(locLeg);
 
