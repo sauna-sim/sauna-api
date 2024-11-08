1442f3968cb6c016d361ba428a93157a1948c87b
Caspian Merlin <caspian@merlins.online>
2024-11-08 14:53:53 +0000

    Remove The Game

--- NavData-Interface/DataSources/CombinedSource.cs
+++ NavData-Interface/DataSources/CombinedSource.cs
@@ -50,7 +50,6 @@ namespace NavData_Interface.DataSources
         public bool AddSource(DataSource source)
         {
             var lastPriority = _sources.Keys[_sources.Keys.Count - 1];
-            throw new Exception("You have lost The Game.");
             return AddSource(source, lastPriority);
         }
 
